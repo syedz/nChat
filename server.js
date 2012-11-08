bf62cf6f396d86145bc0bb83eabe693a56b45066
Zeeshan Syed <zee@hakstudio.com>
2012-11-08 14:33:39 -0500

    Implemented chat using javascript console and exit message

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ var io = require('socket.io'),
   connect = require('connect');
 
 var app = connect().use(connect.static('public')).listen(3000);
+
+// All chatters(sockets) in chatroom
 var chatroom = io.listen(app);
 
 chatroom.sockets.on('connection', function(socket){
-	socket.sockets.emit('entrance', {
+	// Only individual chatters (sockets) will see this message
+	socket.emit('entrance', {
+		message: 'Welcome to the chatroom.'
+	});
+
+	socket.on('disconnect', function(){
+		chatroom.sockets.emit('exit', {
+			message: 'A chatter has disconnected.'
+		});
+	});
+
+	socket.on('chat', function(data){
+		chatroom.sockets.emit('chat', {
+			message: '#' + data.message
+		});
+	});
+
+	chatroom.sockets.emit('entrance', {
 		message: 'A new chatter is online.'
 	});
 });
